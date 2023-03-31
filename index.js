075a069a3d65c279ea67044a5dec7e043f7538bd
HEGGIX <glebermakovich@gmail.com>
2023-04-01 01:10:12 +0300

    feat: add burger-menu

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-// const doc= document.querySelector('link[rel="import"]').import;
-// const text = doc.querySelector('.body');
-// document.body.appendChild(text.cloneNode(true));
+const burger =  document.querySelector(".header-burger");
+const menu = document.querySelector(".nav-conatiner");
+const closeItem = document.querySelector(".burger-close");
+const body = document.querySelector(".body");
+const opacityBurger = document.querySelector(".opacity");
+
+function showBurger(){
+    burger.addEventListener("click",() => {
+       menu.classList.add("nav-conatiner_active");
+       burger.classList.add("header-burger_unactive");
+       body.classList.add("body_active");
+       opacityBurger.classList.add("opacity_active");
+
+    })
+    closeItem.addEventListener("click",() => {
+        menu.classList.remove("nav-conatiner_active");
+        burger.classList.remove("header-burger_unactive");
+        body.classList.remove("body_active");
+        opacityBurger.classList.remove("opacity_active");
+    })
+}
+showBurger()
